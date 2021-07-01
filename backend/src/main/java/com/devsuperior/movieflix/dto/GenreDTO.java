89b246920c705f2bae27d5ace281b9c90d7276f9
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-07-01 19:23:10 -0300

    Implementing DTO classes

---
+++ backend/src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -0,0 +1,58 @@
+package com.devsuperior.movieflix.dto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+
+public class GenreDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	
+	private List<MovieDTO> movies = new ArrayList<>();
+	
+	public GenreDTO() {
+	}
+
+	public GenreDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public GenreDTO(Genre entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+	
+	public GenreDTO(Genre entity, Set<Movie> movies) {
+		id = entity.getId();
+		name = entity.getName();
+		movies.forEach(movie -> this.movies.add(new MovieDTO(movie)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<MovieDTO> getMovies() {
+		return movies;
+	}
+}
